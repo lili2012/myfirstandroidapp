a6a8447b02f2b5cba16326bb49356780b35d4a62
shulian <shuyan2@outlook.com>
2023-08-21 00:10:28 +0800

    finish ch3

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -2,16 +2,20 @@ package com.bignerdranch.android.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.bignerdranch.android.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     //private lateinit var trueButton: Button
     //private lateinit var falseButton: Button
     private lateinit var binding: ActivityMainBinding
     private var currentIndex = 0
+
     private fun setQuestion(){
         var questionTextResId = questions[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
@@ -45,6 +49,30 @@ class MainActivity : AppCompatActivity() {
             setQuestion()
         }
         setQuestion()
+        Log.d(TAG, "onCreate(Bundle?) called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 
     private val questions = listOf(
